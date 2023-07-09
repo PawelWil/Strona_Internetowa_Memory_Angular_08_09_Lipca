f48a6c50e9764b42dc0230ce570981ed02fc53ea
wiltos <pawel.wiltos@gmail.com>
2023-07-09 14:19:59 +0200

    Fiszki z kategorii

--- src/app/ui/category-card/category-card.component.ts
+++ src/app/ui/category-card/category-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {faComments, faPlay, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {Category} from "../../models/category";
 
 @Component({
   selector: 'app-category-card',
@@ -8,7 +9,7 @@ import {faComments, faPlay, faTrash} from "@fortawesome/free-solid-svg-icons";
 })
 
 export class CategoryCardComponent {
-  @Input() categoryName?: string;
+  @Input() category!: Category;
   @Output() removeClicked = new EventEmitter<void>();
 
   trashIcon = faTrash;
